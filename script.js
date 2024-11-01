e5a1211fbd3a10a3a4ed0c0f6bc5acd17703ccb2
Andranik1133 <ando.aleksanyan93@gmail.com>
2024-11-01 04:22:02 +0400

    sticky navigation

--- script.js
+++ script.js
@@ -51,3 +51,58 @@ document.querySelectorAll(".nav__link").forEach(function (el) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
+
+//  Tabbed component
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  // guard clause
+  if (!clicked) return;
+
+  // remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+
+  // active tab
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+const handleHover = function (e, opacity) { 
+  
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+// menu fade animation
+nav.addEventListener("mouseover", function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener("mouseout", function (e) {
+  handleHover(e, 1);
+});
+
+
+// sticky navigation 
+const initialCoords = section1.getBoundingClientRect()
+
+window.addEventListener('scroll', function () {
+  if(window.scrollY > initialCoords.top){
+    nav.classList.add('sticky')
+  }else{
+    nav.classList.remove('sticky')
+  }
+})
